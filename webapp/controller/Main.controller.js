adb2bff98a6ff2c440ac4613b51e1351e01a9827
Sree Lakshmi Rishitha <rishithakolikapongu@gmail.com>
2025-07-20 11:45:04 +0530

    Update Main.controller.js

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,1 +1,28 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast",
+  "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
+  "use strict";
+
+  return Controller.extend("eWitness.controller.Main", {
+    onInit: function () {
+      const oData = {
+        statements: [
+          { date: "2025-07-15", category: "Theft" },
+          { date: "2025-07-14", category: "Assault" }
+        ]
+      };
+      this.getView().setModel(new JSONModel(oData));
+    },
+
+    onAddStatement: function () {
+      MessageToast.show("Add Statement – dialog to open");
+    },
+
+    onViewDetails: function () {
+      MessageToast.show("View confidential details – secured");
+    }
+  });
+});
 
