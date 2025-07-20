b208462b0870b9f2e6a80ce7027c34659bf76377
Sree Lakshmi Rishitha <rishithakolikapongu@gmail.com>
2025-07-20 11:42:26 +0530

    Update Component.js

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,1 +1,14 @@
+sap.ui.define(["sap/ui/core/UIComponent"], function (UIComponent) {
+  "use strict";
+
+  return UIComponent.extend("eWitness.Component", {
+    metadata: {
+      manifest: "json",
+    },
+
+    init: function () {
+      UIComponent.prototype.init.apply(this, arguments);
+    },
+  });
+});
 
