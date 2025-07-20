aaff774b2b64dd0f07bdb83cd70964205f9b1fcf
Sree Lakshmi Rishitha <rishithakolikapongu@gmail.com>
2025-07-20 11:43:23 +0530

    Create manifest.js

---
+++ webapp/manifest.js
@@ -0,0 +1,32 @@
+{
+  "sap.app": {
+    "id": "eWitness",
+    "type": "application",
+    "title": "eWitness",
+    "applicationVersion": {
+      "version": "1.0.0"
+    }
+  },
+  "sap.ui": {
+    "technology": "UI5",
+    "icons": {
+      "icon": "",
+      "favIcon": ""
+    }
+  },
+  "sap.ui5": {
+    "rootView": {
+      "viewName": "eWitness.view.Main",
+      "type": "XML",
+      "async": true,
+      "id": "mainView"
+    },
+    "dependencies": {
+      "minUI5Version": "1.71",
+      "libs": {
+        "sap.m": {}
+      }
+    },
+    "models": {}
+  }
+}
